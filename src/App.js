5c274f548227714f00e67bb53380d45009eaf86f
rinko120305 <haduchieu01@gmail.coml>
2026-03-29 17:44:48 +0700

    update counter button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-    <h1>VH-16</h1>
-    </>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      flexDirection: "column"
+    }}>
+      <h1>Counter React</h1>
+      <p>Số: {count}</p>
+
+      <button 
+        style={{
+          fontSize: "24px",
+          padding: "15px 40px",
+          cursor: "pointer"
+        }}
+        onClick={() => setCount(count + 1)}
+      >
+        Tăng
+      </button>
+    </div>
   );
 }
 
